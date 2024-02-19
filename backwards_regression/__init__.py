62411a83e66a267454ac8c36e3e943afb6ee4ee5
knowusuboaky <64245732+knowusuboaky@users.noreply.github.com>
2024-02-19 09:14:43 -0500

    Delete backwards_regression/__init__.py

--- backwards_regression/__init__.py
+++
@@ -1,8 +0,0 @@
-from .logistic_model import backward_regression as fit_logistic_regression
-from .linear_model import backward_regression as fit_linear_regression
-
-
-__all__ = [
-    'fit_logistic_regression',
-    'fit_linear_regression'
-]
